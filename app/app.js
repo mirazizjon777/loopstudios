d6f358362930197a4ba317f399ecd13335a2c00d
mirazizjon777 <mirazizjonisroilov1@gmail.com>
2023-01-26 01:45:38 +0500

    res

--- app/app.js
+++ app/app.js
@@ -1,7 +1,3 @@
 document.querySelector(".burger-btn").addEventListener("click", () => {
   document.querySelector("header").classList.toggle("dark-mode");
 });
-
-// document.querySelector(".mb-btn").addEventListener("click", () => {
-//   document.querySelector(".main-bottom__btn").classList.toggle("dark2");
-// });
