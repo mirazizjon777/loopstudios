7bb7c17de72d1ba5b4acbf8f8d2ecf5b4473dca8
mirazizjon777 <mirazizjonisroilov1@gmail.com>
2023-01-26 01:28:05 +0500

    responsive full

---
+++ app/app.js
@@ -0,0 +1,7 @@
+document.querySelector(".burger-btn").addEventListener("click", () => {
+  document.querySelector("header").classList.toggle("dark-mode");
+});
+
+// document.querySelector(".mb-btn").addEventListener("click", () => {
+//   document.querySelector(".main-bottom__btn").classList.toggle("dark2");
+// });
